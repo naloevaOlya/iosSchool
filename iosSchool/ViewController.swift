d8947b45506fc667cb5ad4a085fe662db3c62f5e
naloevaOlya <naloeva.o@gmail.com>
2023-03-13 19:53:41 +0700

    initial commit

---
+++ iosSchool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosSchool
+//
+//  Created by student2 on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
