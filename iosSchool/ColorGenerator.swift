2cf58493a8c9c39f18f78b47a5af6298a5a195d0
naloevaOlya <naloeva.o@gmail.com>
2023-03-14 14:02:21 +0700

    fix swift lint

--- iosSchool/ColorGenerator.swift
+++ iosSchool/ColorGenerator.swift
@@ -11,7 +11,6 @@ enum Brightness: Double {
     case dim = 0.1
     case average = 0.5
     case bright = 1.0
-    
     func description() -> String {
         switch self {
         case .dim:
@@ -29,16 +28,16 @@ struct Color {
     let green: Double
     let blue: Double
     var alpha: Brightness
-    
+
     func descriotion() {
-        print ("R: \(red), G: \(green), B : \(blue), alpha: \(alpha.rawValue)")
+        print("R: \(red), G: \(green), B : \(blue), alpha: \(alpha.rawValue)")
     }
 }
 
 class ColorGenerator {
     let colorCodes: [Double] = [0.0, 51.0, 102.0, 153.0, 204.0, 255.0]
     var alpha: Double
-    
+
     init(alpha: Double) {
         self.alpha = alpha
     }
